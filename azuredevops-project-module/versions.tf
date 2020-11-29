d34b6e6ad1709f76cb4222e349a6e3520524f99a
Chris Thorpe <chrisjthorpe@gmail.com>
2020-11-29 20:20:36 +0000

    Restore versions.tf

---
+++ azuredevops-project-module/versions.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    azuredevops = {
+      source  = "terraform-providers/azuredevops"
+      version = ">= 0.0.1"
+    }
+  }
+  required_version = ">= 0.13"
+}
